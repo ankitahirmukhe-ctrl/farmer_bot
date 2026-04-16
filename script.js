28136a7f60cca7ceb50d30163184091623786445
ankitahirmukhe-ctrl <ankitahirmukhe@gmail.com>
2026-04-16 23:56:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ async function sendMessage() {
     try {
         const lang = (currentLang || "English").trim();
 
-      const response = await fetch(" http://localhost:3000", {
+      const response = await fetch("https://farmer-bot-backend-tvva.onrender.com", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ message: text, language: lang, sessionId })
